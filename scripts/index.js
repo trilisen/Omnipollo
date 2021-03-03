b9d94e89907dba5964462ee69fcc36570a8b2606
Trilisen <jonmcg01@gmail.com>
2021-03-03 14:58:45 +0100

    Added Node.js and parcel and confirmation

--- scripts/index.js
+++ scripts/index.js
@@ -5,10 +5,15 @@ console.log(loadingImage);
 setTimeout(() => {
     loadingImage.classList.remove("spinning");
     loadingImage.classList.add("scale");
-}, 6000)
+}, 3000)
 
 loadingImage.addEventListener('animationend', () => {
-    console.log("hej");
     loadingImage.classList.remove("loadingImage");
     loadingImage.classList.add("backgroundImage");
+})
+
+const yesBtn = document.querySelector(".ageConfirmation div button")
+
+yesBtn.addEventListener('click', (e) => {
+    e.path[2].style.display = "none";
 })
