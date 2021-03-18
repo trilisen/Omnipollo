a0d671f7bc2c3a14839ad5fe6e80c47b851c9a04
Vstranne <victorn87@gmail.com>
2021-03-18 10:25:46 +0100

    cleaned up some comments

--- scripts/index.js
+++ scripts/index.js
@@ -134,27 +134,7 @@ const animateHTML = function() {
     };
   };
   animateHTML().init();
-// document.addEventListener("DOMContentLoaded", function(event) {
-//     document.addEventListener("scroll", function(event) {
-//         const animatedBoxes = document.getElementsByClassName("animated-box");
-//         const windowOffsetTop = window.innerHeight + window.scrollY;
 
-//         Array.prototype.forEach.call(animatedBoxes, (animatedBox) => {
-//             const animatedBoxOffsetTop = animatedBox.offsetTop;
-
-//             if (windowOffsetTop >= animatedBoxOffsetTop) {
-//                 addClass(animatedBox, "fade-in");
-//             }
-//         });
-//     });
-// });
-
-// function addClass(element, className) {
-//     const arrayClasses = element.className.split(" ");
-//     if (arrayClasses.indexOf(className) === -1) {
-//         element.className += " " + className;
-//     }
-// }
 
 
 
